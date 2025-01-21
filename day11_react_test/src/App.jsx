f9e15c241c4f651f7289691c92b393e514facc59
Pranav <raramuri@raramuri-MacBook-Pro.local>
2025-01-21 11:55:15 +0530

    Day11 React Test 21 Jan 2025 10:30PM

---
+++ day11_react_test/src/App.jsx
@@ -0,0 +1,35 @@
+//create a child component which takes number as props and show only that number of boxes when entered into input box of parent and clicked Roll
+import { useState,useRef } from 'react';
+import Child from './Child';
+import './App.css';
+
+const App = () => {
+    const inputref=useRef();
+    const [roll,setRoll] = useState(0); 
+    const [error,setError]=useState("")
+    const handleclick = () => {
+        const inputnum = inputref.current.value;
+        if(inputnum>6){
+            setError("Number should be between 1 to 6")
+        }else{
+        setError("");
+        setRoll(inputnum);}
+    };
+    return (
+        <div className='body-container'>
+            <div className='number-button'>
+                <input type='number' className='number-input' name='number-input' ref={inputref} />
+            <h3 style={{color:"red"}}>{error}</h3>
+                <button className='roll-button' onClick={handleclick}>Roll</button>
+            </div>
+            <div>
+                <Child number={roll} />
+            </div>
+        </div>
+    );
+};
+
+export default App;
+
+
+
