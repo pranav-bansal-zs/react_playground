9523e4133753601cdb8b42d435773184dc6636ff
Pranav <raramuri@raramuri-MacBook-Pro.local>
2025-01-22 18:23:12 +0530

    Day 12 React Test 22 Jan 2025 4PM

---
+++ day12_react_test/src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import UseEffectques from './App'
+
+createRoot(document.getElementById('root')).render(
+    // <Timer/>
+    <UseEffectques/>
+)
