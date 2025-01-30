2e0d69cd484ee36e142f97de779f837227734f05
Pranav <raramuri@raramuri-MacBook-Pro.local>
2025-01-31 03:58:44 +0530

    Modal Assignment by Anirudh

--- src/day10/Day10.jsx
+++ src/day10/Day10.jsx
@@ -1,13 +1,7 @@
-<<<<<<< HEAD:src/day10/Day10.jsx
 import React from 'react'
 import Child from './Child';
 const Day10 = () => {
-=======
 
-import Child from './Child';
-
-const App = () => {
->>>>>>> 9095328 (React test 20 Jan 2025 4:30 PM):day10_react_test/src/App.jsx
     const data = {
         image: 'https://campus.epam.com/static/news/255/self-study_03375353.png',
         title: 'React Test',
