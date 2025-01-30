2eb0b1617bc36f86c9c383b44312cc8831d71410
Pranav <raramuri@raramuri-MacBook-Pro.local>
2025-01-31 03:00:53 +0530

    ic

--- day12_react_test/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import UseEffectques from './App'
-
-createRoot(document.getElementById('root')).render(
-    // <Timer/>
-    <UseEffectques/>
-)
--- day13_react_test/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Modal from './Modal'
-
-createRoot(document.getElementById('root')).render(
-    <Modal/>
-)
-
